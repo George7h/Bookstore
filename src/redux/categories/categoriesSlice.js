84c77b3aca6fbab8908e1e4afa50853dac79db07
George <georgie.george7@gmail.com>
2023-08-29 18:23:21 +0400

    Create slice for categories.

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,14 @@
+// src/redux/categoriesSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = 'Under construction';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    
+  },
+});
+
+export default categoriesSlice.reducer;
