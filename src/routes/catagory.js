04f4a925ca9825d17b467f7a6acc1034f6726b0b
George <georgie.george7@gmail.com>
2023-08-30 17:21:37 +0400

    Use selector to display Category state

--- src/routes/catagory.js
+++ src/routes/catagory.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const CategoriesPage = () => (
-  <div>
-    <h2>Categories Page</h2>
-  </div>
-);
+const CategoriesPage = () => {
+  const categories = useSelector((state) => state.categories);
+
+  return (
+    <div>
+      <h2>Categories Page</h2>
+      <p>{categories}</p>
+    </div>
+  );
+};
 
 export default CategoriesPage;
